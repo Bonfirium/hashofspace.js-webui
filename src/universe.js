8cd9d170fa5741c46b6bc2e3e25e09a51d21c265
NickLatkovich <nicklatkovich@gmail.com>
2018-07-14 05:29:47 +0300

    Deflection

--- src/universe.js
+++ src/universe.js
@@ -1,3 +1,4 @@
+import BN from 'bignumber.js';
 import { Tools, Generators } from 'hashofspace.js-lib';
 import { TEXTURE, textures } from './loader';
 import * as PIXI from 'pixi.js';
@@ -15,10 +16,11 @@ export default class Universe {
 		for (let x = 0, xTo = width / scale; x <= xTo; x++) {
 			for (let y = 0, yTo = height / scale; y <= yTo; y++) {
 				const point = new Tools.Point(x, y);
-				if (Generators.UniverseGenerator.getCell(point).hasSystem) {
+				const { hasSystem, hash } = Generators.UniverseGenerator.getCell(point);
+				if (hasSystem) {
 					const star = new PIXI.Sprite(textures[TEXTURE.STAR]);
-					star.x = x * scale;
-					star.y = y * scale;
+					star.x = x * scale + new BN(`0x${hash.substring(0, 2)}`).toNumber() / 256 * scale;
+					star.y = y * scale + new BN(`0x${hash.substring(2, 4)}`).toNumber() / 256 * scale;
 					star.width = scale;
 					star.height = scale;
 					star.anchor.x = 0.5;
